b6185a150bdb23911955acee90629436a5d7a3fa
Mayank <97348934+mayank-1224@users.noreply.github.com>
2022-05-10 00:02:23 +0530

    Update 09.05_M_17. Letter Combinations of a Phone Number.cpp

--- 09.05_M_17. Letter Combinations of a Phone Number.cpp
+++ 09.05_M_17. Letter Combinations of a Phone Number.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    const vector<string> pad = {"", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
-    
+    const vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+	
     vector<string> letterCombinations(string digits) {
-        if (digits.empty()) return {};
-		vector<string> result;
-        result.push_back("");
-        
-        for(auto digit: digits) {
-            vector<string> tmp;
-            for(auto candidate: pad[digit - '0']) {
-                for(auto s: result) {
-                    tmp.push_back(s + candidate);
-                }
+        if(digits.empty())
+			return {};
+		
+		vector<string> res;
+        res.push_back("");
+   
+        for(auto x: digits)
+		{
+        	vector<string> tmp;
+            for(auto y: pad[x - '0'])
+			{
+                for(auto s: result)
+                    tmp.push_back(s + y);
             }
-            result.swap(tmp);
+            res.swap(tmp);
         }
-        return result;
+        return res;
     }
 };
